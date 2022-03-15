cbdc5ce13f01e1bbe655092f93988c6535663854
gnl <gnl0@qq.com>
2022-03-15 18:36:32 +0800

    update

--- react-vite-css/src/pages/transition/Transition.jsx
+++ react-vite-css/src/pages/transition/Transition.jsx
@@ -3,7 +3,12 @@ import './Transition.css'
 export default function Transition() {
   return (
     <div className={'p-2'}>
-      <div id={'box'} className={'h-20 w-20 bg-green-300'}>
+
+      <div id={'circle'} className={'bg-red-300 rounded rounded-full w-20 h-20'}></div>
+
+      <div id={'circleAni'} className={'bg-blue-300 rounded rounded-full w-20 h-20 mt-2'}></div>
+
+      <div id={'box'} className={'h-20 w-20 bg-green-300 mt-2'}>
       </div>
 
       <div id={'box-ani'} className={'w-20 h-20 bg-red-400 mt-2'}></div>
