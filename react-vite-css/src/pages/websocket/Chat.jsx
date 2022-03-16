bd07113b9a101464dff3504856a3d76185315af4
gnl <gnl0@qq.com>
2022-03-16 22:00:36 +0800

    update websocket

--- react-vite-css/src/pages/websocket/Chat.jsx
+++ react-vite-css/src/pages/websocket/Chat.jsx
@@ -5,88 +5,141 @@ import ws from "./websoket";
 
 export default function Chat() {
 
-  const [val, setVal] = useState(null);
+  const from = '111111'
+  const to = '222222'
 
-  const [receiveMsg, setReceiveMsg] = useState([])
-  const [sendMsg, setSendMsg] = useState([])
+  const msgObj = {
+    from: '',
+    to: '',
+    date: '',
+    type: '',
+    body: '',
+    read: false
+  }
+
+  const [currentId, setCurrentId] = useState()
+  const [input, setInput] = useState();
+  const [msgList, setMsgList] = useState([
+    {
+      from: '111111',
+      to: '222222',
+      date: new Date(),
+      type: 'string',
+      body: 'Hello~ R U there?',
+      read: false
+    },
+    {
+      from: '222222',
+      to: '111111',
+      date: new Date(),
+      type: 'string',
+      body: 'Hi~ I\' here!',
+      read: false
+    }
+  ])
+
+  useEffect(() => {
+    setCurrentId(from)
+  }, [])
+
+  const onInputChange = (evt) => {
+    setInput(evt.target.value)
+  }
 
   const sendClick = () => {
-    setSendMsg([
-      ...sendMsg,
-      val
-    ])
-    ws.send(val)
+    if (input) {
+      // 构建消息
+      const finalMsg = buildMsg({from, to, body: input})
+
+      setMsgList([
+          ...msgList,
+        finalMsg
+      ])
+      console.log(finalMsg)
+    }
+    // ws.send(msg)
+
+
+    // 发送完成 input 置空
+    setInput('')
   }
+
+  const closeClick = () => {
+    // ws.close()
+  }
+
+  const buildMsg = ({from, to, date = new Date(), type = 'string', body, read = false}) => {
+    console.log(body)
+    return {
+      from,
+      to,
+      date,
+      type,
+      body,
+      read,
+    }
+  }
+
+  // useEffect(() => {
+  //   if (input) {
+  //     console.log(input)
+  //   }
+  // }, [input])
 
   const onMessage = () => {
     ws.onmessage = res => {
       console.log('收到消息 ', res.data)
 
-      const receive = React.createElement(ReceiveBox, {msg: res.data, className: 'text-red-500'}, '', '')
+      // const receive = React.createElement(ReceiveBox, {msg: res.data, className: 'text-red-500'}, '', '')
       // ReactDOM.render(
       //   receive,
       //   document.getElementById('receiveBox')
       // )
 
-      setReceiveMsg([
-        ...receiveMsg,
-        res.data
-      ])
+      // setReceiveMsg([
+      //   ...receiveMsg,
+      //   res.data
+      // ])
 
     }
   }
 
-  onMessage()
+  // useEffect(() => {
+  //   console.log('sendMsg ', sendMsg)
+  //
+  // }, [sendMsg])
+  //
+  // useEffect(() => {
+  //   console.log('receiveMsg ', receiveMsg)
+  // }, [receiveMsg])
 
-  useEffect(() => {
-    console.log('sendMsg ', sendMsg)
-    console.log('receiveMsg ', receiveMsg)
 
-  }, [receiveMsg, sendMsg])
-
-  const closeClick = () => {
-    ws.close()
-  }
 
   return (
     <div className={'h-screen p-4'}>
-      <h1 className={'text-center text-3xl mb-4'}>welcome to chatroom</h1>
-      <div className={'space-x-4 flex justify-center items-center m-2'}>
-        <input type={'text'} className={'ring ring-2 rounded focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'} onChange={evt => {setVal(evt.target.value)}} />
-        <button type={'button'} className={'bg-blue-200 hover:bg-blue-400 rounded p-1 font-bold'} onClick={sendClick}>send</button>
+      <h1 className={'text-center text-3xl mb-4'}>ChatRoom</h1>
+      <p className={'text-gray-500 text-center'}>Welcome</p>
 
-        <button type={'button'} className={'bg-red-200 hover:bg-red-400 rounded p-1'} onClick={closeClick}>close</button>
+      <div className={'space-x-4 flex justify-center items-center m-4 rounded rounded-md'}>
+
+        <input id={'sendInput'} type={'text'} className={'ring-gray-200 ring-2 rounded focus:outline-none focus:ring-3 focus:ring-blue-500 h-10'} value={input || ''} onChange={onInputChange} />
+
+        <button type={'button'} className={'bg-blue-300 hover:bg-blue-400 rounded p-1 font-semibold'} onClick={sendClick}>send</button>
+
+        <button type={'button'} className={'bg-red-300 hover:bg-red-400 rounded p-1'} onClick={closeClick}>close</button>
       </div>
 
-      <div className={'bg-indigo-100 flex justify-center items-center p-2 rounded rounded-md'}>
+      <div className={'bg-indigo-100 flex flex-col justify-center items-center p-2 rounded rounded-md'}>
         <div className={'bg-gray-100 h-auto flex flex-col w-96 rounded rounded-md'}>
-          <div id={'receiveBox'} className={'flex m-2 justify-start items-center'}>
-            <div className={'bg-white p-2 rounded rounded-md shadow-xm'}>132</div>
-          </div>
 
-          <div id={'sendBox'} className={'flex m-2 justify-end items-center'}>
-            <div className={'bg-green-300 p-2 rounded rounded-md shadow-xm'}>456</div>
-          </div>
+          {
+            msgList.map((msg, index) => {
+              return msg.from === currentId ?
+                <SendBox key={index} msg={msg.body} /> :
 
-          <div id={'receiveBox'} className={'flex m-2 justify-start items-center'}>
-            <div className={'bg-white p-2 rounded rounded-md shadow-xm'}>132</div>
-          </div>
-
-          <div id={'receiveBox'} className={'flex m-2 justify-start items-center'}>
-            <div className={'bg-white p-2 rounded rounded-md shadow-xm'}>qazwsxedc</div>
-          </div>
-
-          <div id={'sendBox'} className={'flex m-2 justify-end items-center'}>
-            <div className={'bg-green-300 p-2 rounded rounded-md shadow-xm'}>456</div>
-          </div>
-
-          <div id={'sendBox'} className={'flex m-2 justify-end items-center'}>
-            <div className={'bg-green-300 p-2 rounded rounded-md shadow-xm'}>456</div>
-          </div>
-
-          <div id={'sendBox'} className={'flex m-2 justify-end items-center'}>
-            <div className={'bg-green-300 p-2 rounded rounded-md shadow-xm'}>456</div>
-          </div>
+                <ReceiveBox key={index} msg={msg.body} />
+            })
+          }
 
         </div>
       </div>
@@ -94,13 +147,19 @@ export default function Chat() {
   )
 }
 
-function ReceiveBox(props) {
-
-  const {msg, className} = props
-
+function SendBox(props) {
   return (
-    <div className={['border-2 border', className].join(' ')}>
-      {msg}
+    <div className={'flex m-2 justify-end items-center'}>
+      <div className={'bg-green-300 p-2 rounded rounded-md shadow-xm'}>{props.msg}</div>
+    </div>
+  )
+}
+
+function ReceiveBox(props) {
+  const {msg} = props
+  return (
+    <div className={'flex m-2 justify-start items-center'}>
+      <div className={'bg-white p-2 rounded rounded-md shadow-xm'}>{msg}</div>
     </div>
   )
 }
